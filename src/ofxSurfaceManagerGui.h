e6e683d74e7d0e2ec2b75486e603fbaabee6fe27
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2014-05-12 10:13:35 +0200

    Add surface manager gui

---
+++ src/ofxSurfaceManagerGui.h
@@ -0,0 +1,11 @@
+#ifndef H_OFX_SURFACE_MANAGER_GUI
+#define H_OFX_SURFACE_MANAGER_GUI
+
+class ofxSurfaceManagerGui
+{
+public:
+    ofxSurfaceManagerGui();
+    ~ofxSurfaceManagerGui();
+};
+
+#endif
