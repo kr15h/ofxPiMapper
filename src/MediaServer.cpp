e33c42cb035b46caeff0b959b0cc380b3b8809c5
Felix Dubrownik <fdu@informatik.uni-kiel.de>
2014-09-13 21:37:45 +0200

    copied init from ofxIO dirWatcher example

--- src/MediaServer.cpp
+++ src/MediaServer.cpp
@@ -7,3 +7,16 @@
 //
 
 #include "MediaServer.h"
+
+namespace ofx {
+namespace piMapper {
+MediaServer::MediaServer() {
+  videoWatcher.registerAllEvents(this);
+  videoWatcher.addPath(ofToDataPath(DEFAULT_VIDEOS_DIR, true), true,
+                       &videoFileFilter);
+  imageWatcher.registerAllEvents(this);
+  imageWatcher.addPath(ofToDataPath(DEFAULT_IMAGES_DIR, true), true,
+                       &imageFileFilter);
+}
+}
+}
--- src/MediaServer.h
+++ src/MediaServer.h
@@ -8,11 +8,26 @@
 
 #pragma once
 
+#include "ofMain.h"
+#include "ofxIO.h"
+
+#define DEFAULT_IMAGES_DIR "sources/images/"
+#define DEFAULT_VIDEOS_DIR "sources/videos/"
+
 namespace ofx {
 namespace piMapper {
 class MediaServer {
  public:
+  MediaServer();
+
  private:
+  // Video
+  ofx::IO::DirectoryWatcherManager videoWatcher;
+  ofx::IO::HiddenFileFilter videoFileFilter;
+
+  // Images
+  ofx::IO::DirectoryWatcherManager imageWatcher;
+  ofx::IO::HiddenFileFilter imageFileFilter;
 };
 }
 }
