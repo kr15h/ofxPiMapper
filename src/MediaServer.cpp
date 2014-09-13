9150e6832fb2f945c3541d89ee8751400516da59
Felix Dubrownik <fdu@informatik.uni-kiel.de>
2014-09-13 21:02:33 +0200

    init, created class

---
+++ src/MediaServer.cpp
@@ -0,0 +1,9 @@
+//
+//  MediaServer.cpp
+//  example
+//
+//  Created by felix on 13.09.14.
+//
+//
+
+#include "MediaServer.h"
---
+++ src/MediaServer.h
@@ -0,0 +1,18 @@
+//
+//  MediaServer.h
+//  example
+//
+//  Created by felix on 13.09.14.
+//
+//
+
+#pragma once
+
+namespace ofx {
+namespace piMapper {
+class MediaServer {
+ public:
+ private:
+};
+}
+}
