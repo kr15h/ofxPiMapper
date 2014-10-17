911650c5531352311e07354d59bd1deb54cf4952
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2014-10-17 22:34:46 +0200

    Add hint to ofxOMXPlayer workaround

--- src/Sources/VideoSource.h
+++ src/Sources/VideoSource.h
@@ -3,6 +3,22 @@
 #include "ofMain.h"
 #include "BaseSource.h"
 #ifdef TARGET_RASPBERRY_PI
+  /*
+   Notice that if you get an error like this:
+   fatal error: libavcodec/opt.h: No such file or directory
+   Create a file opt.h in addons/ofxOMXPlayer/libs/ffmpeg/libavcodec/
+   with the following contents: 
+   
+   #ifndef AVCODEC_OPT_H
+   #define AVCODEC_OPT_H
+   #include "libavcodec/version.h"
+   #if FF_API_OPT_H
+   #include "libavutil/opt.h"
+   #endif
+   #endif // AVCODEC_OPT_H
+   
+   More here: https://github.com/jvcleave/ofxOMXPlayer/issues/34
+  */
   #include "ofxOMXPlayer.h"
 #endif
 
