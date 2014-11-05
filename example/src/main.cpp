dde58195761e930cee31a7feea063bae9613187a
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2014-11-05 21:12:59 +0100

    Add -f flag option to RPi version of he example

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,12 +1,34 @@
 #include "ofMain.h"
 #include "ofApp.h"
+#include <string>
 
-int main()
-{
 #ifdef TARGET_RASPBERRY_PI
-	ofSetupOpenGL(600, 500, OF_FULLSCREEN);
-#else
-    ofSetupOpenGL(600, 500, OF_WINDOW);
-#endif
-	ofRunApp(new ofApp());
+// Accept arguments in the Pi version
+int main(int argc, char* argv[]) {
+  
+  bool fullscreen = false;
+  
+  if (argc > 0) {
+    std::string fullscreenFlag = "-f";
+    for (int i = 0; i < argc; i++) {
+      if (strcmp(argv[i], fullscreenFlag.c_str()) == 0) {
+        fullscreen = true;
+        break;
+      }
+    }
+  }
+  
+  if (fullscreen) {
+    ofSetupOpenGL(600, 500, OF_FULLSCREEN);
+  } else {
+    ofSetupOpenGL(800, 450, OF_WINDOW);
+  }
+  
+  ofRunApp(new ofApp());
 }
+#else
+int main() {
+  ofSetupOpenGL(800, 600, OF_WINDOW);
+  ofRunApp(new ofApp());
+}
+#endif
