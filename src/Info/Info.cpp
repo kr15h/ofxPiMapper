0e8956952923d86f33087620ed35217f8a600cc2
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2016-02-04 16:06:14 +0100

    Update info screen with new shortcuts

--- src/Info/Info.cpp
+++ src/Info/Info.cpp
@@ -14,14 +14,21 @@ void Info::draw(){
 		ss << " 1. Presentation mode\n";
 		ss << " 2. Texture mapping mode\n";
 		ss << " 3. Projection mapping mode\n";
+		ss << "  - Press <,> and <.> to select previous or next surface\n";
+		ss << "  - Press <t> to add new triangle surface\n";
+		ss << "  - Press <q> to add new quad surface\n";
+		ss << "  - Press <p> to toggle perspective warping while quad surface selected\n";
+		ss << "  - Press <g> to add new grid surface\n";
+		ss << "  - Press <[> and <]> to remove or add columns to selected grid surface\n";
+		ss << "  - Press <{> and <}> to remove or add rows to selected grid surface\n";
 		ss << " 4. Source selection mode\n\n";
+		
 		ss << "You can switch between the modes by using <1>, <2>, <3> and <4> "
 			"keys on the keyboard.\n\n";
-		ss << "Press <t> to add new triangle surface\n";
-		ss << "Press <q> to add new quad surface\n";
-		ss << "Press <p> to toggle perspective warping while quad surface selected\n";
+		
 		ss << "Press <s> to save the composition\n";
 		ss << "Press <f> to toggle fullscreen\n";
+		ss << "Press <z> to undo\n";
 		ss << "Press <i> to hide this message";
 		ofDrawBitmapStringHighlight(ss.str(), 10, 20,
 									ofColor(0, 0, 0, 100),
