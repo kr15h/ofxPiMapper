32f539304e2e49ae560225787c91b6822a88cd25
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2016-09-03 17:47:33 +0200

    Add shortcuts to info

--- src/Info/Info.cpp
+++ src/Info/Info.cpp
@@ -13,6 +13,8 @@ void Info::draw(){
 		ss << "There are 4 modes:\n\n";
 		ss << " 1. Presentation mode\n";
 		ss << " 2. Texture mapping mode\n";
+		ss << "  - Press \"<\" and \">\" to select previous or next vertex\n";
+		ss << "  - Press <SPACE> to play/pause the video\n";
 		ss << " 3. Projection mapping mode\n";
 		ss << "  - Press <,> and <.> to select previous or next surface\n";
 		ss << "  - Press \"<\" and \">\" to select previous or next vertex\n";
@@ -24,7 +26,9 @@ void Info::draw(){
 		ss << "  - Press <{> and <}> to remove or add rows to selected grid surface\n";
 		ss << "  - Press <+> and <-> to scale surface up and down\n";
 		ss << "  - Press <9> and <0> to move selected surface one layer up or down\n";
-		ss << "  - Press <z> to undo\n";
+		ss << "  - Press <BACKSPACE> to delete selection\n";
+		ss << "  - Press <SPACE> to play/pause the video\n";
+		ss << "  - Type  <new> to clear composition\n";
 		ss << " 4. Source selection mode\n\n";
 		
 		ss << "You can switch between the modes by using <1>, <2>, <3> and <4> "
