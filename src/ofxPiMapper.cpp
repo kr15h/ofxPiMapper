fdf39c8f7b6590970fe7aefe05c4e61446d7f6b2
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2016-10-08 22:58:11 +0200

    Simplify ofxPiMapper::setNextPreset

--- src/ofxPiMapper.cpp
+++ src/ofxPiMapper.cpp
@@ -27,13 +27,7 @@ void ofxPiMapper::setActivePreset(unsigned int i){
 }
 
 void ofxPiMapper::setNextPreset(){
-	unsigned int currentActivePreset = _application.getSurfaceManager()->getActivePresetIndex();
-	if(currentActivePreset >= _application.getSurfaceManager()->getNumPresets() - 1){
-		currentActivePreset = 0;
-	}else{
-		currentActivePreset += 1;
-	}
-	_application.setActivePreset(currentActivePreset);
+	_application.setNextPreset();
 }
 
 void ofxPiMapper::cloneActivePreset(){
