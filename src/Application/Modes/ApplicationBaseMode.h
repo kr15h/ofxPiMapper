a8a47892fa688c8a4152e114496794ec60dbf826
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2016-10-27 18:22:25 +0300

    Add ApplicationBaseMode::moveSelection empty virtual method

--- src/Application/Modes/ApplicationBaseMode.h
+++ src/Application/Modes/ApplicationBaseMode.h
@@ -36,6 +36,7 @@ class ApplicationBaseMode {
 		virtual void selectSurface(Application * app, int i){}
 		virtual void selectNextSurface(Application * app){}
 		virtual void selectPrevSurface(Application * app){}
+		virtual void moveSelection(Application * app, ofVec2f by){}
 
 };
 
