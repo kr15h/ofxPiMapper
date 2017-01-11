4687e756cef943d36433e4e13db00885174aa5cb
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2017-01-11 23:26:07 +0100

    Add example-custom-shorcuts base source files

---
+++ example-custom-shortcuts/src/ofApp.cpp
@@ -0,0 +1,34 @@
+#include "ofApp.h"
+
+void ofApp::setup(){
+	ofBackground(0);
+	piMapper.setup();
+}
+
+void ofApp::update(){
+	piMapper.update();
+}
+
+void ofApp::draw(){
+	piMapper.draw();
+}
+
+void ofApp::keyPressed(int key){
+	piMapper.keyPressed(key);
+}
+
+void ofApp::keyReleased(int key){
+	piMapper.keyReleased(key);
+}
+
+void ofApp::mousePressed(int x, int y, int button){
+	piMapper.mousePressed(x, y, button);
+}
+
+void ofApp::mouseReleased(int x, int y, int button){
+	piMapper.mouseReleased(x, y, button);
+}
+
+void ofApp::mouseDragged(int x, int y, int button){
+	piMapper.mouseDragged(x, y, button);
+}
