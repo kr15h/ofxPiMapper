c182d96b103666550ae3c727a2bda555c5e57bf3
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2017-01-20 19:59:40 +0200

    Remove obsolete includes from ofxPiMapper.h

--- src/ofxPiMapper.h
+++ src/ofxPiMapper.h
@@ -1,28 +1,8 @@
 #pragma once
 
 #include "ofMain.h"
-#include "SurfaceManager.h"
-#include "MediaServer.h"
 #include "FboSource.h"
-#include "BaseCmd.h"
-#include "CmdManager.h"
-#include "RmSurfaceCmd.h"
 #include "Application.h"
-#include "Info.h"
-
-#include "PresentationMode.h"
-#include "TextureMappingMode.h"
-#include "ProjectionMappingMode.h"
-#include "SourceSelectionMode.h"
-
-#include "BaseSource.h"
-#include "SourceType.h"
-
-namespace ofx {
-namespace piMapper {
-	class Application;
-}
-}
 
 class ofxPiMapper {
 	public:
