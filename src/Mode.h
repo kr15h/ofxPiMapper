c52269a8932e15ec608e3f42201a0723f625508e
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2017-01-21 20:55:04 +0200

    Add Mode.h for storing ofxPiMapper mode enum

---
+++ src/Mode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace ofx {
+namespace piMapper {
+
+enum Mode {
+	PRESENTATION_MODE,
+	TEXTURE_MODE,
+	MAPPING_MODE,
+	SOURCE_MODE
+};
+
+} // namespace piMapper
+} // namespace ofx
