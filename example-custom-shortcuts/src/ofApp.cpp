293c859ef1f9c7ac1bcda57c74d2ef9bfec428fe
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2017-01-22 12:28:29 +0200

    Add custom left, right, up, down moveSelection values

--- example-custom-shortcuts/src/ofApp.cpp
+++ example-custom-shortcuts/src/ofApp.cpp
@@ -32,6 +32,14 @@ void ofApp::keyPressed(int key){
 		mapper.createSurface(ofx::piMapper::TRIANGLE_SURFACE);
 	}else if(key == OF_KEY_BACKSPACE){
 		mapper.eraseSurface(mapper.getNumSurfaces() - 1);
+	}else if(key == OF_KEY_LEFT){
+		mapper.moveSelection(ofVec2f(-20.0f, 0.0f));
+	}else if(key == OF_KEY_RIGHT){
+		mapper.moveSelection(ofVec2f(20.0f, 0.0f));
+	}else if(key == OF_KEY_UP){
+		mapper.moveSelection(ofVec2f(0.0f, -20.0f));
+	}else if(key == OF_KEY_DOWN){
+		mapper.moveSelection(ofVec2f(0.0f, 20.0f));
 	}else{
 		mapper.keyPressed(key);
 	}
