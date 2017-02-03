73abf8fca4ea37c0ed61efbd20d711f4f6486c30
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2017-02-03 22:03:18 +0100

    Add gamepad example initial src files

---
+++ example-gamepad/src/ofApp.cpp
@@ -0,0 +1,34 @@
+#include "ofApp.h"
+
+void ofApp::setup(){
+	ofBackground(0);
+	mapper.setup();
+}
+
+void ofApp::update(){
+	mapper.update();
+}
+
+void ofApp::draw(){
+	mapper.draw();
+}
+
+void ofApp::keyPressed(int key){
+	mapper.keyPressed(key);
+}
+
+void ofApp::keyReleased(int key){
+	mapper.keyReleased(key);
+}
+
+void ofApp::mousePressed(int x, int y, int button){
+	mapper.mousePressed(x, y, button);
+}
+
+void ofApp::mouseDragged(int x, int y, int button){
+	mapper.mouseDragged(x, y, button);
+}
+
+void ofApp::mouseReleased(int x, int y, int button){
+	piMapper.mouseReleased(x, y, button);
+}
