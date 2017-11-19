96ef55afbd74a425ca62f94f28402e364e5fb27d
Theodoros Papatheodorou <theo@randomquark.com>
2017-11-19 11:00:15 +0000

    created simpler example

---
+++ example_simpler/src/ofApp.cpp
@@ -0,0 +1,55 @@
+#include "ofApp.h"
+
+void ofApp::setup(){
+	ofBackground(0);
+
+	// Enable or disable audio for video sources globally
+	// Set this to false to save resources on the Raspberry Pi
+	ofx::piMapper::VideoSource::enableAudio = true;
+	ofx::piMapper::VideoSource::useHDMIForAudio = false;
+
+	// Add our CustomSource to list of fbo sources of the piMapper
+	// FBO sources should be added before piMapper.setup() so the
+	// piMapper is able to load the source if it is assigned to
+	// a surface in XML settings.
+	customSource = new CustomSource();
+	piMapper.registerFboSource(customSource);
+	piMapper.setup();
+
+	// The info layer is hidden by default, press <i> to toggle
+	// piMapper.showInfo();
+	
+	ofSetFullscreen(Settings::instance()->getFullscreen());
+	ofSetEscapeQuitsApp(false);
+
+    dummyObjects.load("dummy-objects.png");
+}
+
+void ofApp::update(){
+	piMapper.update();
+}
+
+void ofApp::draw(){
+    dummyObjects.draw(200,200);
+    piMapper.draw();
+}
+
+void ofApp::keyPressed(int key){
+	piMapper.keyPressed(key);
+}
+
+void ofApp::keyReleased(int key){
+	piMapper.keyReleased(key);
+}
+
+void ofApp::mousePressed(int x, int y, int button){
+	piMapper.mousePressed(x, y, button);
+}
+
+void ofApp::mouseReleased(int x, int y, int button){
+	piMapper.mouseReleased(x, y, button);
+}
+
+void ofApp::mouseDragged(int x, int y, int button){
+	piMapper.mouseDragged(x, y, button);
+}
