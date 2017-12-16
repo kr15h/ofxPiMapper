6f298161c6046ac41378ab67e7b57d2a8b4e965f
c-mendoza <c@matadata.com>
2017-12-15 22:58:09 -0500

    Example update for RPi
    Example update for RPi

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -14,9 +14,25 @@ int main(int argc, char * argv[]){
 			break;
 		}
 	}
-	
-	Settings::instance()->setFullscreen(fullscreen);
 
-	ofSetupOpenGL(1024, 768, OF_WINDOW);
-	ofRunApp(new ofApp());
+	shared_ptr<ofAppBaseWindow> window;
+#ifdef TARGET_OPENGLES
+	ofGLESWindowSettings esSettings;
+	esSettings.glesVersion = 2;
+	esSettings.width = 1440;
+	esSettings.height = 900;
+	window = ofCreateWindow(esSettings);
+#else
+	ofGLWindowSettings glSettings;
+	glSettings.glVersionMajor = 2;
+	glSettings.glVersionMinor = 1;
+	glSettings.width = 1440;
+	glSettings.height = 900;
+	window = ofCreateWindow(glSettings);
+#endif
+
+	auto app = std::make_shared<ofApp>();
+	ofRunApp(window, app);
+	Settings::instance()->setFullscreen(fullscreen);
+	ofRunMainLoop();
 }
