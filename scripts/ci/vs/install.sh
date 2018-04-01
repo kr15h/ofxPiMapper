651f431c7e84e45def2122d637e70805386e0f1a
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2018-04-01 11:26:24 +0200

    Remove AppVeyor template vars from ci sh scripts

--- scripts/ci/vs/install.sh
+++ scripts/ci/vs/install.sh
@@ -2,11 +2,11 @@
 
 pwd
 git clone --depth=1 -b of-master-patch https://github.com/kr15h/ofxJSON addons/ofxJSON 
-mv addons/%APPVEYOR_PROJECT_NAME%/example_basic/addons.make.norpi addons/%APPVEYOR_PROJECT_NAME%/example_basic/addons.make
-mv addons/%APPVEYOR_PROJECT_NAME%/example_camera/addons.make.norpi addons/%APPVEYOR_PROJECT_NAME%/example_camera/addons.make
-mv addons/%APPVEYOR_PROJECT_NAME%/example_fbo-sources/addons.make.norpi addons/%APPVEYOR_PROJECT_NAME%/example_fbo-sources/addons.make
-mv addons/%APPVEYOR_PROJECT_NAME%/example_pocketvj/addons.make.norpi addons/%APPVEYOR_PROJECT_NAME%/example_pocketvj/addons.make
-mv addons/%APPVEYOR_PROJECT_NAME%/example_remote-client/addons.make.norpi addons/%APPVEYOR_PROJECT_NAME%/example_remote-client/addons.make
-mv addons/%APPVEYOR_PROJECT_NAME%/example_remote-server/addons.make.norpi addons/%APPVEYOR_PROJECT_NAME%/example_remote-server/addons.make
-mv addons/%APPVEYOR_PROJECT_NAME%/example_simpler/addons.make.norpi addons/%APPVEYOR_PROJECT_NAME%/example_simpler/addons.make
-rm -rf addons/%APPVEYOR_PROJECT_NAME%/example_gamepad
+mv addons/ofxPiMapper/example_basic/addons.make.norpi addons/ofxPiMapper/example_basic/addons.make
+mv addons/ofxPiMapper/example_camera/addons.make.norpi addons/ofxPiMapper/example_camera/addons.make
+mv addons/ofxPiMapper/example_fbo-sources/addons.make.norpi addons/ofxPiMapper/example_fbo-sources/addons.make
+mv addons/ofxPiMapper/example_pocketvj/addons.make.norpi addons/ofxPiMapper/example_pocketvj/addons.make
+mv addons/ofxPiMapper/example_remote-client/addons.make.norpi addons/ofxPiMapper/example_remote-client/addons.make
+mv addons/ofxPiMapper/example_remote-server/addons.make.norpi addons/ofxPiMapper/example_remote-server/addons.make
+mv addons/ofxPiMapper/example_simpler/addons.make.norpi addons/ofxPiMapper/example_simpler/addons.make
+rm -rf addons/ofxPiMapper/example_gamepad
