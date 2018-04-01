c061c29a12a0bc33e29962c8600c9eda2542d3c4
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2018-04-01 13:57:17 +0200

    Compile only half of the examles on AppVeyor because of the 60m limit

--- scripts/ci/vs/install.sh
+++ scripts/ci/vs/install.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+echo "Custom ofxPiMapper CI install script for VS"
 pwd
 git clone --depth=1 -b of-master-patch https://github.com/kr15h/ofxJSON addons/ofxJSON 
+
+# Test only part of examples because uf the 60m time limit on AppVeyor
 mv addons/ofxPiMapper/example_basic/addons.make.norpi addons/ofxPiMapper/example_basic/addons.make
-mv addons/ofxPiMapper/example_camera/addons.make.norpi addons/ofxPiMapper/example_camera/addons.make
 mv addons/ofxPiMapper/example_fbo-sources/addons.make.norpi addons/ofxPiMapper/example_fbo-sources/addons.make
-mv addons/ofxPiMapper/example_pocketvj/addons.make.norpi addons/ofxPiMapper/example_pocketvj/addons.make
 mv addons/ofxPiMapper/example_remote-client/addons.make.norpi addons/ofxPiMapper/example_remote-client/addons.make
 mv addons/ofxPiMapper/example_remote-server/addons.make.norpi addons/ofxPiMapper/example_remote-server/addons.make
-mv addons/ofxPiMapper/example_simpler/addons.make.norpi addons/ofxPiMapper/example_simpler/addons.make
+
+# Delete the rest
 rm -rf addons/ofxPiMapper/example_gamepad
+rm -rf addons/ofxPiMapper/example_pocketvj
+rm -rf addons/ofxPiMapper/example_simpler
+rm -rf addons/ofxPiMapper/example_camera
+echo "Custom install script done"
