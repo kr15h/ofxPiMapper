6bc5b1d950fff83bfee9b3300f5408b3b559cf56
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2018-05-16 01:39:46 +0200

    Update timeout with -k 0

--- scripts/ci/rpi/build-script.sh
+++ scripts/ci/rpi/build-script.sh
@@ -4,6 +4,6 @@ echo "Begin build-script.sh"
 
 cd /home/pi/openFrameworks/addons/ofxPiMapper/example_basic
 timeout --version
-timeout --signal=INT 30m make -j $(nproc)
+timeout -k 0 30m make -j $(nproc)
 
 echo "End build-script.sh"
