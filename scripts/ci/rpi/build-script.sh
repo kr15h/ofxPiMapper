87f7b49c3689cf1c17446de73211e9cf7f20ae51
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2018-05-16 01:28:18 +0200

    Specify SIGINT for thimeout operation to quit make on time

--- scripts/ci/rpi/build-script.sh
+++ scripts/ci/rpi/build-script.sh
@@ -3,6 +3,7 @@
 echo "Begin build-script.sh"
 
 cd /home/pi/openFrameworks/addons/ofxPiMapper/example_basic
-timeout -k 30m make -j $(nproc)
+timeout --version
+timeout --signal=INT 30m make -j $(nproc)
 
 echo "End build-script.sh"
