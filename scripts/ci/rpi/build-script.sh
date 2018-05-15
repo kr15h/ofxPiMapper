212695449b47949f06a83c072c51d9770761d161
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2018-05-15 22:35:56 +0200

    Attempt to fix kill make after timeout

--- scripts/ci/rpi/build-script.sh
+++ scripts/ci/rpi/build-script.sh
@@ -3,6 +3,6 @@
 echo "Begin build-script.sh"
 
 cd /home/pi/openFrameworks/addons/ofxPiMapper/example_basic
-timeout 30m make -j $(nproc)
+timeout -k 30m make -j $(nproc)
 
 echo "End build-script.sh"
