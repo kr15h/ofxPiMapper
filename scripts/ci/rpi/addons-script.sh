1f41efcab49f93080344b9e9aba4c1cfdba4d5fd
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2018-05-15 19:18:45 +0200

    Add another Travis compile pass as another job

--- scripts/ci/rpi/addons-script.sh
+++ scripts/ci/rpi/addons-script.sh
@@ -8,4 +8,17 @@ make -j $(nproc)
 cd /home/pi/openFrameworks/addons/ofxJSON/example_file_read_write
 make -j $(nproc)
 
+echo "Inastalling extra packages."
+sudo apt-get -yq install usbmount dosfstools exfat-fuse exfat-utils
+
+echo "Setting hostname."
+echo "opm" > /etc/hostname
+cat /etc/hostname
+
+echo "Setting up to run on boot."
+crontab -l > mycron
+echo "@reboot /home/pi/openFrameworks/addons/ofxPiMapper/example_basic/bin/example_basic" >> mycron
+crontab mycron
+rm mycron
+
 echo "Compiling addons done!"
--- scripts/ci/rpi/finalize-script.sh
+++ scripts/ci/rpi/finalize-script.sh
@@ -3,19 +3,6 @@
 echo "Finalizing OPM RPi image."
 
 cd /home/pi/openFrameworks/addons/ofxPiMapper/example_basic
-make -j $(nproc)
-
-echo "Inastalling extra packages."
-sudo apt-get -yq install usbmount dosfstools exfat-fuse exfat-utils
-
-echo "Setting hostname."
-echo "opm" > /etc/hostname
-cat /etc/hostname
-
-echo "Setting up to run on boot."
-crontab -l > mycron
-echo "@reboot /home/pi/openFrameworks/addons/ofxPiMapper/example_basic/bin/example_basic" >> mycron
-crontab mycron
-rm mycron
+timeout 25m make -j $(nproc)
 
 echo "OPM RPi image setup done!"
