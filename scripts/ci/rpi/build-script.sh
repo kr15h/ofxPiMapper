b97a2319cddcb3f12ff22aa9dac4817d7b9d89e4
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2018-05-16 08:47:35 +0200

    Another attempt to fix the timeout script

--- scripts/ci/rpi/build-script.sh
+++ scripts/ci/rpi/build-script.sh
@@ -4,6 +4,6 @@ echo "Begin build-script.sh"
 
 cd /home/pi/openFrameworks/addons/ofxPiMapper/example_basic
 timeout --version
-timeout -k 0 30m make -j $(nproc)
+timeout -signal=KILL 1800 make -j $(nproc)
 
 echo "End build-script.sh"
