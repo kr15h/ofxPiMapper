5efd33952de5bdc918830521239637832dffbc96
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2018-05-16 09:30:50 +0200

    Fix wrong signal param

--- scripts/ci/rpi/build-script.sh
+++ scripts/ci/rpi/build-script.sh
@@ -4,6 +4,6 @@ echo "Begin build-script.sh"
 
 cd /home/pi/openFrameworks/addons/ofxPiMapper/example_basic
 timeout --version
-timeout -signal=KILL 1800 make -j $(nproc)
+timeout --signal=KILL 1800 make -j $(nproc)
 
 echo "End build-script.sh"
