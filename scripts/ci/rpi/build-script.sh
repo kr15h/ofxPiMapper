9d22b767b76d9777d59514275da2fd3161d840b7
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2018-05-16 13:59:29 +0200

    Try to fix timeout

--- scripts/ci/rpi/build-script.sh
+++ scripts/ci/rpi/build-script.sh
@@ -2,10 +2,12 @@
 
 echo "Begin build-script.sh"
 
-sudo apt-get -y install coreutils
+sudo apt-get -y install timeout
 
 cd /home/pi/openFrameworks/addons/ofxPiMapper/example_basic
+kill -l
+which timeout
 timeout --version
-sudo timeout --signal=SIGKILL 10 make -j $(nproc)
+/usr/bin/timeout --signal=SIGKILL 10 make -j $(nproc)
 
 echo "End build-script.sh"
