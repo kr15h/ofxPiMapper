bd446153420c1a9fc83454d18c160b7bd9db52d2
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2018-05-16 14:54:59 +0200

    Fix timelimit script

--- scripts/ci/rpi/build-script.sh
+++ scripts/ci/rpi/build-script.sh
@@ -4,6 +4,6 @@ echo "Begin build-script.sh"
 
 sudo apt-get -y install timelimit
 
-/usr/bin/timelimit -s KILL 10 /usr/bin/make -j $(nproc) -C /home/pi/openFrameworks/addons/ofxPiMapper/example_basic
+timelimit -s KILL -t 10 make -j $(nproc) -C /home/pi/openFrameworks/addons/ofxPiMapper/example_basic
 
 echo "End build-script.sh"
