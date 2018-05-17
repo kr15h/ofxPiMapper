8a019f12392e67c6fa5e336ab9ac56b82ea9a3f3
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2018-05-17 12:57:42 +0200

    Remove KILL signal from timelimit and increase time limit

--- scripts/ci/rpi/build-script.sh
+++ scripts/ci/rpi/build-script.sh
@@ -3,7 +3,7 @@
 echo "Begin build-script.sh"
 
 date
-timelimit -s KILL -t 2000 make -j $(nproc) -C /home/pi/openFrameworks/addons/ofxPiMapper/example_basic
+timelimit -t 2300 make -j $(nproc) -C /home/pi/openFrameworks/addons/ofxPiMapper/example_basic
 date
 
 echo "End build-script.sh"
