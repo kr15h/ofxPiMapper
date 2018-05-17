3c01ca2ea173262f9265dab59a9132eea2dee884
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2018-05-17 14:31:26 +0200

    Send SIGTERM (15) instead of kill (9) for timelimit

--- scripts/ci/rpi/build-script.sh
+++ scripts/ci/rpi/build-script.sh
@@ -3,7 +3,8 @@
 echo "Begin build-script.sh"
 
 date
-timelimit -t 2300 make -j $(nproc) -C /home/pi/openFrameworks/addons/ofxPiMapper/example_basic
+timelimit -S 15 -t 2200 make -j $(nproc) -C /home/pi/openFrameworks/addons/ofxPiMapper/example_basic
 date
+sleep 10
 
 echo "End build-script.sh"
