204dc135f80b995cc5d0e6fe402acd227eba52c7
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2018-05-17 20:27:48 +0200

    Change timelimit cmd to SIGINT and reduce timeout to 2100s

--- scripts/ci/rpi/build-script.sh
+++ scripts/ci/rpi/build-script.sh
@@ -3,7 +3,7 @@
 echo "Begin build-script.sh"
 
 date
-timelimit -S 1 -t 2200 make -j $(nproc) -C /home/pi/openFrameworks/addons/ofxPiMapper/example_basic
+timelimit -S 2 -t 2100 make -j $(nproc) -C /home/pi/openFrameworks/addons/ofxPiMapper/example_basic
 date
 sleep 10
 
