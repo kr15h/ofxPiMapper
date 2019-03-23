f0a164f6d31e7f115e59ddbfc5ac663e8cf5c616
toughvj <31914712+toughvj@users.noreply.github.com>
2019-03-23 22:40:30 +0100

    Update main.cpp

--- example_pocketvj/src/main.cpp
+++ example_pocketvj/src/main.cpp
@@ -2,6 +2,8 @@
 #include "ofApp.h"
 
 int main(){
-	ofSetupOpenGL(800, 450, OF_WINDOW);
-	ofRunApp(new ofApp());
+	ofGLFWWindowSettings settings;
+    settings.windowMode = OF_FULLSCREEN;
+    ofCreateWindow(settings);
+    ofRunApp(new ofApp);
 }
